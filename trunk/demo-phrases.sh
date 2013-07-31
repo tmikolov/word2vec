fea071bdb92a59ecdbc959c483d6763dd150a272
tmikolov@google.com <tmikolov@google.com@c84ef02e-58a5-4c83-e53e-41fc32d635eb>
2013-07-31 23:01:50 +0000

    aa

--- trunk/demo-phrases.sh
+++ trunk/demo-phrases.sh
@@ -1,8 +1,9 @@
-
 make clean
 make
-wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-gzip -d text8.gz -f
+if [ ! -e text8 ]; then
+  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
+  gzip -d text8.gz -f
+fi
 time ./word2phrase -train text8 -output text8-phrase -threshold 200 -debug 2
 time ./word2vec -train text8-phrase -output vectors-phrase.bin -cbow 1 -size 200 -window 5 -negative 5 -hs 0 -sample 1e-3 -threads 12 -binary 1
 ./distance vectors-phrase.bin

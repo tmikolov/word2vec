d1b0954fe320456bcbb21988cd971ca959390d3f
tmikolov <tmikolov@google.com>
2013-08-01 19:28:23 +0000

    aa

--- demo-analogy.sh
+++ demo-analogy.sh
@@ -1,4 +1,3 @@
-make clean
 make
 if [ ! -e text8 ]; then
   wget http://mattmahoney.net/dc/text8.zip -O text8.gz
@@ -8,5 +7,5 @@ echo ---------------------------------------------------------------------------
 echo Note that for the word analogy to perform well, the models should be trained on much larger data sets
 echo Example input: paris france berlin
 echo -----------------------------------------------------------------------------------------------------
-time ./word2vec -train text8 -output vectors.bin -cbow 1 -size 200 -window 5 -negative 5 -hs 0 -sample 1e-3 -threads 12 -binary 1
+time ./word2vec -train text8 -output vectors.bin -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
 ./word-analogy vectors.bin

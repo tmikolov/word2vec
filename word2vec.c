dd2d92a30187737c417af389e2e953a9eea07039
tmikolov <tmikolov@google.com>
2013-11-28 06:12:04 +0000

    fixed bug: the counter in K-means was updated too frequently

--- word2vec.c
+++ word2vec.c
@@ -561,10 +561,8 @@ void TrainModel() {
       for (b = 0; b < clcn * layer1_size; b++) cent[b] = 0;
       for (b = 0; b < clcn; b++) centcn[b] = 1;
       for (c = 0; c < vocab_size; c++) {
-        for (d = 0; d < layer1_size; d++) {
-          cent[layer1_size * cl[c] + d] += syn0[c * layer1_size + d];
-          centcn[cl[c]]++;
-        }
+        for (d = 0; d < layer1_size; d++) cent[layer1_size * cl[c] + d] += syn0[c * layer1_size + d];
+        centcn[cl[c]]++;
       }
       for (b = 0; b < clcn; b++) {
         closev = 0;
